f96dda5bf189a481caf0b469fbc282f05fa9d97a
Bryan Clark <bclark2@umbc.edu>
2018-08-06 21:59:18 -0700

    Update orderedLinearSearch.py

--- orderedLinearSearch.py
+++ orderedLinearSearch.py
@@ -1,7 +1,7 @@
 #Ordered linear search
 
 mylist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-myterm = 22
+myterm = 21
 
 def search(ordered_list, term): 
    ordered_list_size = len(ordered_list) 
