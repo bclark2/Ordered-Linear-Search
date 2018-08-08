29e23293d19be736e5ad0b487f9365e72c543a8a
Matthew Jenkins <mjenki03@gmail.com>
2018-08-08 12:26:18 -0500

    clean up return, add enumerate

--- orderedLinearSearch.py
+++ orderedLinearSearch.py
@@ -1,19 +1,20 @@
 # Ordered linear search
 
-mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-          12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
-myterm = 21
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+           12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 
 
 def search(ordered_list, term):
-    ordered_list_size = len(ordered_list)
-    for i in range(ordered_list_size):
-        if term == ordered_list[i]:
-            print('Index of my number = ' + ' ' + str(i))
-            print('Number I want to search for = ' + ' ' + str(my_term))
-        elif ordered_list[i] > term:
-            return None
+    for index, item in enumerate(ordered_list):
+        if term == item:
+            return (
+                f'Index of my number = {index}\n'
+                f'Number I want to search for = {term}\n')
+        elif item > term:
+            break
 
-    return None
+    return f'{term} not found\n'
 
-search(my_list, my_term)
+print(search(my_list, 20))
+print(search(my_list, 0))
+print(search(my_list, 4))
